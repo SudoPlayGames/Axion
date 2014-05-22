81e4c4920b1536c381490368dca488e32b3da2c3
Jason Taylor <jason@codetaylor.com>
2014-05-22 13:41:21 -0700

    Fixed string output adapter and added test for it

--- src/com/sudoplay/axion/adapter/impl/StringOutputAdapter.java
+++ src/com/sudoplay/axion/adapter/impl/StringOutputAdapter.java
@@ -1,11 +1,12 @@
 package com.sudoplay.axion.adapter.impl;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import com.sudoplay.axion.adapter.Interface_Adapter;
 import com.sudoplay.axion.tag.Abstract_Tag;
@@ -16,58 +17,76 @@ public class StringOutputAdapter implements Interface_Adapter {
 
   private class DataOutputWrapper {
 
-    private final DataOutput dataOutput;
+    private final PrintWriter out;
 
-    public DataOutputWrapper(final DataOutput newDataOutput) {
-      dataOutput = newDataOutput;
+    public DataOutputWrapper(final OutputStream newOutputStream, final Charset charset) {
+      out = new PrintWriter(new OutputStreamWriter(newOutputStream, charset), true);
     }
 
     public DataOutputWrapper append(final String toAppend) throws IOException {
-      dataOutput.writeUTF(toAppend);
+      out.print(toAppend);
       return this;
     }
 
-    public DataOutput getDataOutput() {
-      return dataOutput;
+    public DataOutputWrapper endLine() {
+      out.println();
+      return this;
     }
 
   }
 
-  private String indentString = "  ";
+  private static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+  private static final String DEFAULT_INDENT_STRING = "  ";
 
-  public void setIndentString(final String newIndentString) {
+  private final Charset charset;
+  private final String indentString;
+
+  public StringOutputAdapter() {
+    this(DEFAULT_CHARSET, DEFAULT_INDENT_STRING);
+  }
+
+  public StringOutputAdapter(final String newIndentString) {
+    this(DEFAULT_CHARSET, newIndentString);
+  }
+
+  public StringOutputAdapter(final Charset newCharset) {
+    this(newCharset, DEFAULT_INDENT_STRING);
+  }
+
+  public StringOutputAdapter(final Charset newCharset, final String newIndentString) {
+    charset = newCharset;
     indentString = newIndentString;
   }
 
   @Override
-  public Abstract_Tag read(Abstract_Tag parent, DataInput in) throws IOException {
+  public Abstract_Tag read(Abstract_Tag parent, InputStream in) throws IOException {
     throw new UnsupportedOperationException();
   }
 
   @Override
-  public void write(Abstract_Tag tag, DataOutput out) throws IOException {
+  public void write(Abstract_Tag tag, OutputStream out) throws IOException {
     int indent = 0;
-    _format(tag, new DataOutputWrapper(out), indent);
+    _format(tag, new DataOutputWrapper(out, charset), indent);
   }
 
   private void _format(final Abstract_Tag tag, final DataOutputWrapper out, int indent) throws IOException {
 
-    applyIndent(indent, out).append(tag.toString()).append("\n");
+    applyIndent(indent, out).append(tag.toString()).endLine();
 
     if (tag instanceof TagCompound) {
-      applyIndent(indent, out).append("{").append("\n");
+      applyIndent(indent, out).append("{").endLine();
       indent++;
       for (Abstract_Tag at : ((TagCompound) tag).getAsMap().values()) {
         _format(at, out, indent);
       }
-      applyIndent(indent - 1, out).append("}").append("\n");
+      applyIndent(indent - 1, out).append("}").endLine();
     } else if (tag instanceof TagList) {
-      applyIndent(indent, out).append("{").append("\n");
+      applyIndent(indent, out).append("{").endLine();
       indent++;
       for (Abstract_Tag at : ((TagList) tag).getAsList()) {
         _format(at, out, indent);
       }
-      applyIndent(indent - 1, out).append("}").append("\n");
+      applyIndent(indent - 1, out).append("}").endLine();
     }
   }
 
---
+++ test/com/sudoplay/axion/adapter/impl/StringOutputAdapterTest.java
@@ -0,0 +1,51 @@
+package com.sudoplay.axion.adapter.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.Test;
+
+import com.sudoplay.axion.tag.Abstract_Tag;
+import com.sudoplay.axion.tag.impl.TagCompound;
+
+public class StringOutputAdapterTest {
+
+  @Test
+  public void testRead() throws IOException {
+    try {
+      StringOutputAdapter adapter = new StringOutputAdapter();
+      adapter.read(null, null);
+      fail(StringOutputAdapter.class.getSimpleName() + " should throw UnsupportedOperationException on read()");
+    } catch (UnsupportedOperationException e) {
+      assertTrue(true);
+    }
+  }
+
+  @Test
+  public void testWrite() throws IOException {
+    StringOutputAdapter adapter = new StringOutputAdapter(StandardCharsets.UTF_8);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    adapter.write(getTestTag(), baos);
+    baos.close();
+    assertEquals(getTestTagString(), baos.toString("UTF-8"));
+  }
+
+  private Abstract_Tag getTestTag() {
+    return new TagCompound("root") {
+      {
+        putBoolean("boolean", true);
+        putInt("int", 16);
+      }
+    };
+  }
+
+  private String getTestTagString() {
+    return "TAG_Compound(\"root\"): 2 entries\n{\n  TAG_Int(\"int\"): 16\n  TAG_Byte(\"boolean\"): 1\n}\n";
+  }
+
+}
