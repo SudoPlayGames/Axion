1653af95f20fe2396020cbf8c170119e5727d9ff
Jason Taylor <jason@codetaylor.com>
2014-05-28 17:25:54 -0700

    Added more comments

--- src/com/sudoplay/axion/api/AxionWritable.java
+++ src/com/sudoplay/axion/api/AxionWritable.java
@@ -3,10 +3,37 @@ package com.sudoplay.axion.api;
 import com.sudoplay.axion.Axion;
 import com.sudoplay.axion.tag.Tag;
 
+/**
+ * The {@link AxionWritable} provides an interface for creating classes that can
+ * be easily read and written by {@link Axion}.
+ * 
+ * @author Jason Taylor
+ * 
+ * @param <T>
+ * @see Axion#read(java.io.File, AxionWritable)
+ * @see Axion#read(java.io.InputStream, AxionWritable)
+ * @see Axion#write(AxionWritable, java.io.File)
+ * @see Axion#write(AxionWritable, java.io.OutputStream)
+ */
 public interface AxionWritable<T extends Tag> {
 
+  /**
+   * Creates, writes to, and returns a {@link Tag}.
+   * 
+   * @param axion
+   *          the {@link Axion} instance
+   * @return a {@link Tag}
+   */
   public T write(final Axion axion);
 
+  /**
+   * Reads from a {@link Tag}.
+   * 
+   * @param tag
+   *          the {@link Tag} to read from
+   * @param axion
+   *          the {@link Axion} instance
+   */
   public void read(T tag, final Axion axion);
 
 }
