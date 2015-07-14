e71a097d870f2f3f2239c8d89ae75d94854dc7ee
Jason Taylor <jason@codetaylor.com>
2015-07-13 22:44:46 -0700

    added a couple of factory classes for common functions and predicates

---
+++ src/com/sudoplay/axion/util/AxionFunctions.java
@@ -0,0 +1,16 @@
+package com.sudoplay.axion.util;
+
+import java.util.function.Function;
+
+/**
+ * Common functions.
+ * <p>
+ * Created by Jason Taylor on 7/13/2015.
+ */
+public class AxionFunctions {
+
+  public static <V> Function<V, V> ifNullChangeTo(V defaultValue) {
+    return v -> (v == null) ? defaultValue : v;
+  }
+
+}
---
+++ src/com/sudoplay/axion/util/AxionPredicates.java
@@ -0,0 +1,16 @@
+package com.sudoplay.axion.util;
+
+import java.util.function.Predicate;
+
+/**
+ * Common predicates.
+ * <p>
+ * Created by Jason Taylor on 7/13/2015.
+ */
+public class AxionPredicates {
+
+  public static <O> Predicate<O> notNull() {
+    return o -> o != null;
+  }
+
+}
